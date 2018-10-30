721816b3edb34c78c2f2bdc62bc09fac9fafa59b
AlidaContreras <alidacontreras01@gmail.com>
2018-10-30 17:51:00 -0500

    Grafico de Donas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//graficos
+import { ChartsModule } from 'ng2-charts';
+
 import { AppComponent } from './app.component';
+import { GraficoDonaComponent } from './components/grafico-dona/grafico-dona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficoDonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
