ab8133c0d0c66f40865e1dd7832af66b778840b0
Danielapariona <daniela.parionac@gmail.com>
2018-10-31 15:08:54 -0500

    Estilos del Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { RouterModule, Routes } from "@angular/router";
 import { CuestionarioComponent } from "./components/cuestionario/cuestionario.component";
 import { VistaDonasComponent } from "./components/vista-donas/vista-donas.component";
 import { UsuarioComponent } from "./components/usuario/usuario.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const APP_ROUTES: Routes = [
+  { path: "", component: HomeComponent },
   { path: "conociendo-mi-perfil", component: CuestionarioComponent },
   { path: "mi-perfil", component: VistaDonasComponent },
   { path: "registro", component: UsuarioComponent }
